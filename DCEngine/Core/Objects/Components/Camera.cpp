5d735b9471c97cf24c2c91623d1545db3df76a03
ChenShu96 <chen.shu@digipen.edu>
2015-09-19 12:34:41 -0700

    Update Camera.cpp 9/18
    Added rotation and replace CameraHeight and Width by Size.

--- DCEngine/Core/Objects/Components/Camera.cpp
+++ DCEngine/Core/Objects/Components/Camera.cpp
@@ -2,6 +2,15 @@
 
 namespace DCEngine {
 
+	void Camera::TransformUpdate(glm::vec3 &PositionInput, GLfloat RotationDegree)
+	{
+		Position.x = PositionInput.x;
+		Position.y = PositionInput.y;
+		Position.z = PositionInput.z;
+
+		Roll = RotationDegree;
+	}
+
   glm::mat4 Camera::GetViewMatrix() {
     return glm::lookAt(Position, Position + Front, Up);
   }
@@ -9,22 +18,27 @@ namespace DCEngine {
   void Camera::UpdateCameraVectors() {
     // Calculate the new front vector
     glm::vec3 front;
+	glm::vec3 up;
     front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
     front.y = sin(glm::radians(Pitch));
     front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
     Front = glm::normalize(front);
+
+	up.x = cos(glm::radians(Roll));
+	up.y = sin(glm::radians(Roll));
+	up.z = sin(glm::radians(Roll));
     // Also, recalculate the Right and Up vector
     Right = glm::normalize(glm::cross(Front, WorldUp));
-    Up = glm::normalize(glm::cross(Right, Front));
+    Up = glm::normalize(up);
   }
 
   glm::mat4 Camera::GetProjectionMatrix() {
     if (Projection = Orthographic) {
       //First parameter: left coordinate second: right coordinate third: bottom coordinate fourth: top coordinate fifth: near plane sixthL far plane
-      return glm::ortho(Position.x - CameraWidth / 2, Position.x + CameraWidth / 2, Position.y - CameraHeight / 2, Position.x + CameraHeight / 2, NearPlane, FarPlane);
+      return glm::ortho(Size * 40 / 2, Position.x + Size * 40 / 2, Size * 40 / 2, Size * 40 / 2, NearPlane, FarPlane);
     }
     else if (Projection = Perspective) {
-      return glm::perspective(45.0f, CameraWidth / CameraHeight, 0.1f, 100.0f);
+      return glm::perspective(45.0f, 1.0f, 0.1f, 100.0f);
     }
     else
 
