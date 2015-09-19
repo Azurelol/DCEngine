4b441677a9364aaad358e53acc79bc42ba308cf7
ChenShu96 <chen.shu@digipen.edu>
2015-09-19 13:01:48 -0700

    Sprite.cpp Update 9/19
    Note: 1. BlendMode and SpriteSource 0% (It seems done by Christian?)
             2. Visible, Color, FlipX/Y need Graphics Pipeline. Local prgress done.

--- DCEngine/Core/Objects/Components/Sprite.cpp
+++ DCEngine/Core/Objects/Components/Sprite.cpp
@@ -11,5 +11,66 @@ namespace DCEngine {
     registerEvent->SpriteObj = (GameObject*)Owner(); // SHOULD THIS BE CASTED?
     space_->Dispatch<Events::SpriteRegistration>(registerEvent);
     
+    Visible = true;
+	  Color.x = 1;
+	  Color.y = 1;
+	  Color.z = 1;
+	  _BlendMode = BlendMode::Alpha;
+	  FlipX = false;
+	  FlipY = false;
+	  XFlipped = false;
+	  YFlipped = false;
+	  for (int i = 0; i < 4; i++)//Initialize the flip matrix
+	  {
+		  for (int j = 0; j < 4; j++)
+		  {
+			  if (i == j)
+			  {
+				  FlipMatrix[i][j] = 1;
+			  }
+		  }
+	  }
+  }
+  
+  void Sprite::UpdateFlip()
+  {
+	  if (FlipX == true)
+	  {
+		  if (XFlipped == false)
+		  {
+			  FlipMatrix[0][0] = -1;
+			  XFlipped = true;
+		  }
+	  }
+	  else
+	  {
+		  if (XFlipped == true) 
+		  {
+			  FlipMatrix[0][0] = 1;
+			  XFlipped = false;
+		  }
+	  }
+
+	  if (FlipY == true)
+	  {
+		  if (YFlipped == false)
+		  {
+			  FlipMatrix[1][1] = 1;
+			  YFlipped = true;
+		  }
+	  }
+	  else
+	  {
+		  if (YFlipped == true)
+		  {
+			  FlipMatrix[1][1] = -1;
+			  YFlipped = false;
+		  }
+	  }
+  }
+
+  void Sprite::UpdateSprite()
+  {
+	  UpdateFlip();
   }
 }
