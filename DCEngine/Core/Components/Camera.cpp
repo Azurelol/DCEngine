d48e1128357896677ea84218e33bba896deb0805
ChenShu96 <chen.shu@digipen.edu>
2015-09-21 14:32:45 -0700

    Update Camera.cpp 9/21
    Get transform information from Object

--- DCEngine/Core/Components/Camera.cpp
+++ DCEngine/Core/Components/Camera.cpp
@@ -4,25 +4,31 @@
 
 namespace DCEngine {
 
-	void Camera::TransformUpdate(glm::vec3 &PositionInput, GLfloat RotationDegree)
-	{
-		Position.x = PositionInput.x;
-		Position.y = PositionInput.y;
-		Position.z = PositionInput.z;
-
-		Roll = RotationDegree;
-	}
-
   void Camera::Initialize() {
     auto gameObjOwner = (GameObject*)Owner();
-    auto transform = gameObjOwner->getComponent<Transform>();
-    
+	if (Transform_ != gameObjOwner->getComponent<Transform>())
+	{
+	  trace << "Camera: Intialize failed. No Transform component";
+	}
+	Yaw = -90.0f;
+	Pitch = 0.0f;
+	Roll = 0.0f;
 
-
+	Front = glm::vec3(0.0f, 0.0f, -1.0f);
+	Up = glm::vec3(0.0f, 1.0f, 0.0f);
+	Right = glm::vec3(1.0f, 0.0f, 0.0f);
   }
 
+  void Camera::Update() 
+  {
+	Roll = Transform_->Rotation.z;
+	UpdateCameraVectors();
+  }
+
+
+
   glm::mat4 Camera::GetViewMatrix() {
-    return glm::lookAt(Position, Position + Front, Up);
+    return glm::lookAt(Transform_->Translation, Transform_->Translation + Front, Up);
   }
 
   void Camera::UpdateCameraVectors() {
@@ -38,14 +44,14 @@ namespace DCEngine {
 	up.y = sin(glm::radians(Roll));
 	up.z = sin(glm::radians(Roll));
     // Also, recalculate the Right and Up vector
-    Right = glm::normalize(glm::cross(Front, WorldUp));
+    Right = glm::normalize(glm::cross(Front, Up));
     Up = glm::normalize(up);
   }
 
   glm::mat4 Camera::GetProjectionMatrix() {
     if (Projection = Orthographic) {
       //First parameter: left coordinate second: right coordinate third: bottom coordinate fourth: top coordinate fifth: near plane sixthL far plane
-      return glm::ortho(Size * 40 / 2, Position.x + Size * 40 / 2, Size * 40 / 2, Size * 40 / 2, NearPlane, FarPlane);
+      return glm::ortho(Size * 40 / 2, Transform_->Translation.x + Size * 40 / 2, Size * 40 / 2, Size * 40 / 2, NearPlane, FarPlane);
     }
     else if (Projection = Perspective) {
       return glm::perspective(45.0f, 1.0f, 0.1f, 100.0f);
