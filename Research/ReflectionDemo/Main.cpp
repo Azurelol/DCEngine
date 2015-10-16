fa2524ce4e624e24593c763cb68a97d0d46693f7
Azurelol <c.sagel@digipen.edu>
2015-10-16 08:52:05 -0700

    Ay

---
+++ Research/ReflectionDemo/Main.cpp
@@ -0,0 +1,11 @@
+#include "Classes.h"
+#include <iostream>
+int main() {
+
+
+
+  // End of demo
+  std::cout << "End of reflection demo\n";
+  std::cin.get();
+  return 0;
+}
---
+++ Research/ReflectionDemo/Reflection.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// Libraries
+
+// Headers
+#include "MetaData.h"
+#include "Meta.h"
+
+class System {
+public:
+  System() {}
+};
+
+class Reflection : public System {
+
+public:
+  Reflection() {}
+  void Initialize() {}
+  void Update(float dt) {}
+  void Terminate() {}
+
+private:
+
+
+};
+
+
+  
