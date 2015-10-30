e5a07eedb3391d769bc60173c1a8504fc197f472
Azurelol <c.sagel@digipen.edu>
2015-10-30 11:31:11 -0700

    Removed research solution

--- Research/ccreflect/openSolution.bat
+++
@@ -1,8 +0,0 @@
-cd premake
-premake4 clean
-premake4 vs2010
-cd ..
-cd vs
-cd vs2010
-START "" CCReflect.sln
-cd ..
--- Research/ccreflect/src/CCReflect/LuaInterface/CCLuaIncludes.h
+++
@@ -1,8 +0,0 @@
-extern "C"
-{
-  #include <lua.h>
-  #include <lua.hpp>
-  #include <lauxlib.h>
-  #include <luaconf.h>
-  #include <lualib.h>
-}
