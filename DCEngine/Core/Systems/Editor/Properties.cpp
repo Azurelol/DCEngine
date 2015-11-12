aca3810825034f595b65f931e592254ac0ad03db
Azurelol <c.sagel@digipen.edu>
2015-11-12 15:57:13 -0800

    Breaking the build on push = quality dev

--- DCEngine/Core/Systems/Editor/Properties.cpp
+++ DCEngine/Core/Systems/Editor/Properties.cpp
@@ -102,8 +102,8 @@ namespace DCEngine {
 
         // If the bound field/property does not have the Property Attribute, do
         // nothing.
-        if (!property->HasAttribute("Property"))
-          continue;
+        //if (!property->HasAttribute("Property"))
+        //  continue;
 
         auto state = Daisy->getSystem<Reflection>()->Handler()->getState();
         
@@ -114,79 +114,80 @@ namespace DCEngine {
 
         // Property-getter
         Zilch::Call getCall(property->Get, Daisy->getSystem<Reflection>()->Handler()->getState());
-        getCall.SetHandle(Zilch::Call::This, component.get());
+        Component *aPointer = component.get();
+        getCall.SetHandle(Zilch::Call::This, aPointer);
         // Property-setter
         Zilch::Call setCall(property->Set, Daisy->getSystem<Reflection>()->Handler()->getState());
-        getCall.SetHandle(Zilch::Call::This, component.get());
+        setCall.SetHandle(Zilch::Call::This, aPointer);
               
 
         // Property: Boolean
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Boolean))) {
           auto boolean = getCall.Get<Zilch::Boolean>(Zilch::Call::Return);
           ImGui::Checkbox(property->Name.c_str(), &boolean);
-          setCall.Set(0, boolean);
+          //setCall.Set(0, boolean);
         }
         // Property: String
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::String))) {
-          auto string = getCall.Get<Zilch::String>(Zilch::Call::Return).c_str();
+          auto string = getCall.Get<Zilch::String>(Zilch::Call::Return);
           char buf[32];
-          strcpy(buf, string);
+          strcpy(buf, string.c_str());
           ImGui::InputText(property->Name.c_str(), buf, IM_ARRAYSIZE(buf));
-          setCall.Set(0, buf);
+          //setCall.Set(0, buf);
         }
         // Property: Int
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Integer))) {
           auto integer = getCall.Get<Zilch::Integer>(Zilch::Call::Return);
           ImGui::InputInt(property->Name.c_str(), &integer);
-          setCall.Set(0, integer);
+          //setCall.Set(0, integer);
         }
         // Property: Int2
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Integer2))) {
           auto integer2 = getCall.Get<Zilch::Integer2>(Zilch::Call::Return);
           int int2[2] = { integer2.x, integer2.y };
           ImGui::InputInt2(property->Name.c_str(), int2);
-          setCall.Set(0, int2);
+          //setCall.Set(0, Zilch::Integer2(int2[0], int2[1]));
         }
         // Property: Int3
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Integer3))) {
           auto integer3 = getCall.Get<Zilch::Integer3>(Zilch::Call::Return);
           int int3[3] = { integer3.x, integer3.y, integer3.z };
           ImGui::InputInt3(property->Name.c_str(), int3);
-          setCall.Set(0, int3);
+          //setCall.Set(0, int3);
         }
         // Property: Int4
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Integer4))) {
           auto integer4 = getCall.Get<Zilch::Integer4>(Zilch::Call::Return);
           int int4[4] = { integer4.x, integer4.y, integer4.z, integer4.w};
           ImGui::InputInt4(property->Name.c_str(), int4);
-          setCall.Set(0, int4);
+          //setCall.Set(0, int4);
         }
         // Property: Real (float)
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Real))) {
           auto real = getCall.Get<Zilch::Real>(Zilch::Call::Return);
           ImGui::InputFloat(property->Name.c_str(), &real, 0.1f);
-          setCall.Set(0, real);
+          //setCall.Set(0, real);
         }
         // Property: Real2 (Vec2)
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Real2))) {
           auto vec2 = getCall.Get<Zilch::Real2>(Zilch::Call::Return);
           float vec2f[2] = { vec2.x, vec2.y };
           ImGui::InputFloat2(property->Name.c_str(), vec2f);
-          setCall.Set(0, vec2f);
+         // setCall.Set(0, vec2f);
         }
         // Property: Real3 (Vec3)
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Real3))) {
           auto vec3 = getCall.Get<Zilch::Real3>(Zilch::Call::Return);
           float vec3f[3] = { vec3.x, vec3.y, vec3.z };
           ImGui::InputFloat2(property->Name.c_str(), vec3f);
-          setCall.Set(0, vec3f);
+          //setCall.Set(0, vec3f);
         }
         // Property: Real4 (Vec4)
         if (Zilch::Type::IsSame(property->PropertyType, ZilchTypeId(Zilch::Real4))) {
           auto vec4 = getCall.Get<Zilch::Real4>(Zilch::Call::Return);
           float vec4f[4] = { vec4.x, vec4.y, vec4.z, vec4.w};
           ImGui::InputFloat2(property->Name.c_str(), vec4f);
-          setCall.Set(0, vec4f);
+          //setCall.Set(0, vec4f);
         }
       }
     }
