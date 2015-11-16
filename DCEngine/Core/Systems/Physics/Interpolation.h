d2e2edb4aae6598cb19145486ed07ed7d7985085
Blaine Riner <blaine.reiner@digipen.edu>
2015-11-16 13:26:53 -0800

    redid a tiny bit of interpolation

--- DCEngine/Core/Systems/Physics/Interpolation.h
+++ DCEngine/Core/Systems/Physics/Interpolation.h
@@ -2,9 +2,9 @@
 
 namespace DCEngine
 {
-  // Linnear Interpolation functions
-  float Interpolate(float start, float end, float ratio);
-  Vec3 Interpolate(Vec3 start, Vec3 end, float ratio);
+  // Linnear Interpolation function
+  template <typename T>
+  T Interpolate(const T &start, const T &end, float ratio);
 
 }
 
