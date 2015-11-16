edc9a4d45fea23d2c5c6055d15352e4e62501707
Blaine Riner <blaine.reiner@digipen.edu>
2015-11-16 12:47:33 -0800

    added linnear interpolation functions needs testing to see if works properly

---
+++ DCEngine/Core/Systems/Physics/Interpolation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace DCEngine
+{
+  // Linnear Interpolation functions
+  float Interpolate(float start, float end, float ratio);
+  Vec3 Interpolate(Vec3 start, Vec3 end, float ratio);
+
+}
+
