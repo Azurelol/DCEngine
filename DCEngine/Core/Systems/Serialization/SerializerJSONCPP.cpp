a1eb96750b232ccd511d8aba9d0763c534d30be2
Blaine Riner <blaine.reiner@digipen.edu>
2015-11-23 15:54:28 -0800

    good news everyone

--- DCEngine/Core/Systems/Serialization/SerializerJSONCPP.cpp
+++ DCEngine/Core/Systems/Serialization/SerializerJSONCPP.cpp
@@ -1,3 +1,12 @@
+/*****************************************************************************/
+/*!
+@file   SerializerJSONCPP.cpp
+@author Christian Sagel
+@par    email: c.sagel\@digipen.edu
+@date   11/19/2015
+@copyright Copyright 2015, DigiPen Institute of Technology. All rights reserved.
+*/
+/******************************************************************************/
 #include "SerializerJSONCPP.h"
 #include "../../Objects/ObjectsInclude.h"
 #include "../System.h"
