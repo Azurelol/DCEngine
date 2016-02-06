e3f2120cb6fb028d9ca48c6a44c6dd691e6864a8
Christian Sagel <c.sagel@digipen.edu>
2016-02-05 22:11:25 -0800

    That too.

---
+++ DCEngine/Core/Systems/Editor/MultiSelector.cpp
@@ -0,0 +1,25 @@
+
+
+namespace DCEngine {
+  namespace Systems {
+
+    class MultiSelector {
+    public:
+
+
+    private:
+      bool MultiSelectDragging = false;
+      Vec4 MultiSelectColor = Vec4(0.3, 0.7, 0.3f, 0.5f);
+
+      Vec3 MultiSelectMidpoint;
+      Vec3 MultiSelectStartPos;
+      Vec3 MultiSelectArea;
+
+      Vec3  SelectedBoundingCenter;
+      float SelectedBoundingWidth;
+      float SelectedBoundingHeight;
+
+    };
+
+  }
+}
