8a391649fe39f60d9ede9df1cc7b988e4ff1ff46
vedrfolnir <jasonjorgen@gmail.com>
2016-02-29 22:20:51 -0800

    Added debug coloring for state changes in grunt for John.

--- DCEngine/Projects/Rebound/Components/AI/Grunt.h
+++ DCEngine/Projects/Rebound/Components/AI/Grunt.h
@@ -39,6 +39,9 @@ namespace DCEngine {
       float AttackJumpStrengthX;
       float AttackJumpStrengthY;
       float AttackJumpPeriod;
+      Vec4 IdleColor = Vec4(0, 1, 0, 1);
+      Vec4 PatrolColor = Vec4(0, 0, 1, 1);
+      Vec4 AttackColor = Vec4(1, 0, 0, 1);
 
       
 
@@ -55,6 +58,9 @@ namespace DCEngine {
       DCE_DEFINE_PROPERTY(float, AttackJumpStrengthX);
       DCE_DEFINE_PROPERTY(float, AttackJumpStrengthY);
       DCE_DEFINE_PROPERTY(float, AttackJumpPeriod);
+      DCE_DEFINE_PROPERTY(Vec4, IdleColor);
+      DCE_DEFINE_PROPERTY(Vec4, PatrolColor);
+      DCE_DEFINE_PROPERTY(Vec4, AttackColor);
 
       // Methods
       Grunt(Entity& owner) : Component(std::string("Grunt"), owner) {}
